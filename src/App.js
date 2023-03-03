8d059c2e45279e252b510d78940ea2b7b633c257
ChenFeijian <534688737@qq.com>
2023-03-03 22:25:00 +0800

    项目搭建-配置路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Layout'
+import Login from './pages/Login'
+
 function App() {
-  return <div className="App">App根组件</div>
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/home" component={Home}></Route>
+          <Route path="/login" component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
